4577d8960f0271c3f0bd5b1712165a5057be265a
Nick Potiriadis <32678324+NickyPot@users.noreply.github.com>
2019-11-08 12:53:20 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace serverMessaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
