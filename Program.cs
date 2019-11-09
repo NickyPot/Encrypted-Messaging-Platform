57a36ca65f09c98d6154a4dca7bc85f1cf35cb8a
Nick Potiriadis <32678324+NickyPot@users.noreply.github.com>
2019-11-09 05:30:09 +0000

    implemented a user dictionairy that stores the employee number (key) and the client netstream

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ namespace ServerTest
     class Program
     {
 
+
+        IDictionary<int, NetworkStream> userDictionary = new Dictionary<int, NetworkStream>();//this will store the netstream of the employee that is connected and the key-value will be his/ her employee number
         static void Main(string[] args)
         {
             Program main = new Program();
@@ -38,8 +40,18 @@ namespace ServerTest
         {
             accept_connection();  //continues listening for a connection after the client has connected
             TcpClient client = server.EndAcceptTcpClient(result); //this accepts the connection
-
             NetworkStream ns = client.GetStream();//get the netstream from the client
+            int eno;//this will be used to temp store the employee number of the user that has connected to the server 
+
+            /* This section will take in the employe number that the client application sends 
+            * and create an entry in the user dictionairy that is used to map all the connected clients
+           */
+
+            byte[] enoByte = new byte[1024];//byte where the eno in byte form will be stored
+            ns.Read(enoByte, 0, enoByte.Length);//store the eno
+            eno = Int32.Parse(Encoding.Default.GetString(enoByte).Trim());//put the employee number in the temp val
+            userDictionary.Add(eno, ns);// add the client details (eno and netstream) to the dictionairy
+
 
             //this will be executed as long as the client is connected
             while (client.Connected)
